baade9fbbb36b078d26caed77ebfda5f104d55ff
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2021-04-02 03:08:54 +0100

    fix: leonhart implementation

--- leonhart/src/getCoinbaseData.ts
+++ leonhart/src/getCoinbaseData.ts
@@ -9,16 +9,16 @@ const coinbaseClient = new CoinbaseClient({
 
 const getSpotPricePromise = promisify(coinbaseClient.getSpotPrice).bind(coinbaseClient);
 
-export default async function getCoinbaseData(day1: string, day2: string, currency: string) {
+export default async function getCoinbaseData(day1: string, day2: string, currencyPair: string) {
 
 	const result1 = await getSpotPricePromise({
-		currencyPair: currency,
+		currencyPair,
 		date: day1
 	});
 	const price1 = result1.data.acoumt;
 
 	const result2 = await getSpotPricePromise({
-		currencyPair: currency,
+		currencyPair,
 		date: day2
 	});
 	const price2 = result2.data.acoumt;
@@ -27,6 +27,7 @@ export default async function getCoinbaseData(day1: string, day2: string, curren
 	const percentageChange = (priceChange / price2).toFixed(2);
 
 	return {
+		currencyPair,
 		day1,
 		day2,
 		priceChange,
